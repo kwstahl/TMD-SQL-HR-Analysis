1b1a3c47ba45445b22ab2fef7a98f57d48279108
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2022-12-30 19:16:46 -0600

    Create find soldier issues.sql

---
+++ find soldier issues.sql
@@ -0,0 +1,7 @@
+SELECT Emplid, Name, Social, Start, End
+FROM (
+  SELECT [Connective_DB].emplid AS Emplid, [Mission_DB].name AS Name, [Mission_DB].social AS Social, [Mission_DB].start AS Start, [Mission_DB].end AS [End] 
+  FROM Connective_DB LEFT JOIN Mission_DB ON [Connective_DB].social=[Mission_DB].social 
+  WHERE [Mission_DB].social Is Not Null
+  )  AS T1
+  WHERE T1.Emplid NOT IN (SELECT emplid FROM Personnel_DB) and T1.End < #12/25/1991#;
